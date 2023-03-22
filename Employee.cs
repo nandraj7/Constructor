cb16e27a6257d970974e2139cae6bdd6ceba9b48
hp <hp@HPELITEBOOK840G>
2023-03-22 19:43:26 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Employee
+    {
+        public Employee()       //Parameterless or Default Constructor
+        {
+            this.Id = 10;
+            this.Name = "Akshay";
+            this.Salary = 10000;
+        
+        }
+        public Employee(int id, string name, int salary)    //Parameterized Constructor
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+
+        }
+
+        public Employee(Employee emp)   //Copy Constructor
+        {
+            this.Id = emp.Id;
+            this.Name = emp.Name;
+            this.Salary = emp.Salary;
+        }
+
+        public void PutData()
+        {
+            Console.WriteLine("Id =" +this.Id);
+            Console.WriteLine("Name =" + this.Name);
+            Console.WriteLine("Salary =" + this.Salary);
+
+        }
+
+        #region Type Casting Demo
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+        public string GetName()
+        {
+            return "abc";
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using Constructor;
+public class Program
+{
+    public static void Main()
+    {
+ 
+
+        Employee emp1 = new Employee();     //Call To Parameterless Consturctor
+        emp1.PutData();
+
+        Employee emp2 = new Employee(11,"Vijay",11000);     //Call To Parameterized Consturctor
+        emp2.PutData();
+
+        Employee emp3 = new Employee(12,"Amit",12000);
+        emp3.PutData();
+
+        Employee emp2Copy = new Employee(emp2);      //Call To Copy Consturctor
+        emp2Copy.PutData();
+
+    }
+}
