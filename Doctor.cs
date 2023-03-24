64e52aa12ecb80d55748801db94142aa3e1a97f3
hp <hp@HPELITEBOOK840G>
2023-03-24 13:17:12 +0530

    doctor

---
+++ Doctor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+
+        public static int NumbersOfDoctors { get; set; }
+        static Doctor()
+        {
+            Doctor.NumbersOfDoctors = 5;
+        }
+
+        public Doctor()
+        {
+            Console.WriteLine(" ");
+            this.Id = 01;
+            this.Name = "Murliprasad Sharma";
+            this.Degree = "MBBS";
+        }
+        public Doctor(int Id, string Name, string Degree)
+        {
+            Console.WriteLine(" ");
+            this.Id = Id;
+            this.Name = Name;
+            this.Degree = Degree;
+        }
+        public Doctor(Doctor doctor)
+        {
+            Console.WriteLine(" ");
+            this.Id = doctor.Id;
+            this.Name = doctor.Name;
+            this.Degree = doctor.Degree;
+
+        }  
+        public void ShowData()
+        {
+            Console.WriteLine("Id = " + this.Id);
+            Console.WriteLine("Name = " + this.Name);
+            Console.WriteLine("Degree = " + this.Degree);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,19 +6,34 @@ public class Program
 {
     public static void Main()
     {
- 
+        Doctor doctor1 = new Doctor();
+        doctor1.ShowData();
+        Doctor doctor2 = new Doctor(21,"Neel","BAMS");
+        doctor2.ShowData();
+        Doctor doctor3 = new Doctor(22,"Nitin","MD");
+        doctor3.ShowData();
+        Doctor doctor4 = new Doctor(23,"Mukesh","BDS");
+        doctor4.ShowData();
+        Doctor doctor5 = new Doctor(30,"Mahesh","MBBS");
+        doctor5.ShowData();
 
-        Employee emp1 = new Employee();     //Call To Parameterless Consturctor
-        emp1.PutData();
+        Doctor newdoctor = new Doctor(doctor5);
+        newdoctor.ShowData();
 
-        Employee emp2 = new Employee(11,"Vijay",11000);     //Call To Parameterized Consturctor
-        emp2.PutData();
+        //Employee emp1 = new Employee();     //Call To Parameterless Consturctor
+        //emp1.PutData();
 
-        Employee emp3 = new Employee(12,"Amit",12000);
-        emp3.PutData();
+        //Employee emp2 = new Employee(11,"Vijay",11000);     //Call To Parameterized Consturctor
+        //emp2.PutData();
 
-        Employee emp2Copy = new Employee(emp2);      //Call To Copy Consturctor
-        emp2Copy.PutData();
+        //Employee emp3 = new Employee(12,"Amit",12000);
+        //emp3.PutData();
+
+        //Employee emp2Copy = new Employee(emp2);      //Call To Copy Consturctor
+        //emp2Copy.PutData();
+
+
+
 
     }
 }
